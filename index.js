8bd935f7a58e2fa2345bc5f9b79fb7a89d6d1888
yaischyk <98265143+yaischyk@users.noreply.github.com>
2022-10-12 21:34:52 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Request = require('./models/Request');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/create', (req, res) => {
+    const makeid = () => {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      
+        for (var i = 0; i < 18; i++)
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+      
+        return text;
+    }
+
+    const makeId = makeid();
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+      
+    const newRequest = new Request({
+        requestId: makeId,
+        sendCrypto: req.query.sendCrypto ? req.query.sendCrypto : 'No data',
+        getCrypto: req.query.getCrypto ? req.query.getCrypto : 'No data',
+        sendAmount: req.query.sendAmount ? req.query.sendAmount : 'No data',
+        getAmount: req.query.getAmount ? req.query.getAmount : 'No data',
+        card: req.query.card ? req.query.card : 'No data',
+        cardName: req.query.cardName ? req.query.cardName : 'No data',
+        telegram: req.query.telegram ? req.query.telegram : 'No data',
+        date: Date.now(),
+        num: getRandomInt(999999)
+    });
+    newRequest.save();
+      
+    res.json({result: makeId})
+});
+
+app.get('/request', async (req, res) => {
+    const id = req.query.id;
+    const data = await Request.find({requestId: id})
+    res.json(data)
+});
+
+
+
+const mongooseConnection = async () => {
+    await mongoose.connect('mongodb+srv://admin:sukapizdec22@cluster0.hpmjcg6.mongodb.net/?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+}
+  
+mongooseConnection();
+  
+app.listen(3001, async () => {
+    console.log('🚀 Server started...');
+});
